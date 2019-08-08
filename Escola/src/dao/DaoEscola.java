6ca2b055fe4c0997f6cfe65019c5c35058efd941
Administrador <Administrador@10.10.4.10>
2019-08-08 16:44:33 -0300

    nuevo

---
+++ Escola/src/dao/DaoEscola.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Escola;
+/**
+ *
+ * @author Administrador
+ */
+public class DaoEscola {
+     public static boolean inserir(Escola objeto) {
+        String sql = "INSERT INTO Escola (nome, endereco, sigla) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getNome());
+            ps.setString(2, objeto.getEndereco());
+            ps.setString(3, objeto.getSigla());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+     public static void main(String[] args) {
+        Escola objeto = new Escola();
+        objeto.setNome("Sinodal");
+        objeto.setEndereco("Ibirubá");
+        objeto.setSigla("CSI");
+        
+        boolean resultado = inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+}
